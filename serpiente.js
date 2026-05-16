2f716f3e746d2dbf85fa367bb4efe6a72e3124b8
lgcarrilloc <mgoluiswar@hotmail.com>
2026-05-16 17:01:18 -0500

    Parte 2: pintarParte y pruebas de posición en tablero

--- serpiente.js
+++ serpiente.js
@@ -5,12 +5,18 @@ const TAMANIO_CELDA = 25;
 function dibujarTodo() {
   limpiarCanvas();
   dibujarTablero();
+  pintarParte(5, 5);
+  pintarParte(10, 2);
+  pintarParte(5, 23);
+  pintarParte(23, 15);
+  pintarParte(0, 12);
+  pintarParte(23, 0);
+
 }
 
 function limpiarCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-
 
 function dibujarTablero(){
   ctx.strokeStyle = "#b8d400";
@@ -29,27 +35,11 @@ function dibujarTablero(){
   }
 
 }
-
-
-
-function dibujarCuadricula(){
-  ctx.strokeStyle = "#b8d400";
-  ctx.lineWidth = 0.5;
-  // Líneas verticales
-  for(let x = 0; x <= canvas.width; x += TAMANIO_CELDA){
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvas.height);
-    ctx.stroke();
-  }
-  // Líneas horizontales
-  for(let y = 0; y <= canvas.height; y += TAMANIO_CELDA){
-    ctx.beginPath();
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
-    ctx.stroke();
-  }
-
+function pintarParte(lineaX, lineaY){
+  let x = lineaX * TAMANIO_CELDA;
+  let y = lineaY * TAMANIO_CELDA;
+  ctx.fillStyle = "orange";
+  ctx.fillRect(x, y, TAMANIO_CELDA, TAMANIO_CELDA);
 }
 // ✅ IMPORTANTE: dibujar al cargar, dibujar tablero
 dibujarTodo();
