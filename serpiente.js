6ca83ead0dc63b32d65b69b02c51ce321b41685a
lgcarrilloc <mgoluiswar@hotmail.com>
2026-05-16 16:34:37 -0500

    taller parte1

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,58 @@
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+const TAMANIO_CELDA = 25;
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+function dibujarTodo() {
+  limpiarCanvas();
+  dibujarTablero();
+}
+
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 
-    
+function dibujarTablero(){
+  ctx.strokeStyle = "#b8d400";
+  ctx.lineWidth = 1;
+  for(let x = 0; x <= canvas.width; x += TAMANIO_CELDA){
+    ctx.beginPath();
+    ctx.moveTo(x, 0); 
+    ctx.lineTo(x, canvas.height); 
+    ctx.stroke();
+  }
+  for(let y = 0; y <= canvas.height; y += TAMANIO_CELDA){
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+    ctx.stroke();
+  }
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
+}
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
 
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
+function dibujarCuadricula(){
+  ctx.strokeStyle = "#b8d400";
+  ctx.lineWidth = 0.5;
+  // Líneas verticales
+  for(let x = 0; x <= canvas.width; x += TAMANIO_CELDA){
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+    ctx.stroke();
+  }
+  // Líneas horizontales
+  for(let y = 0; y <= canvas.height; y += TAMANIO_CELDA){
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+    ctx.stroke();
+  }
+
+}
+// ✅ IMPORTANTE: dibujar al cargar, dibujar tablero
+dibujarTodo();
 
 
 
